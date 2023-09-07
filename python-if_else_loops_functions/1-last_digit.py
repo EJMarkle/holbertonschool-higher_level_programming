60a48b95582fbf83f144047ada8712729c8f0a48
EJMarkle <emarkle99@gmail.com>
2023-09-07 12:39:53 -0700

    1...pls...

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,10 @@
 import random
 number = random.randint(-10000, 10000)
 
-lastdig = number % 10
+lastdig = abs(number) % 10
 
+if number < 0:
+    lastdig = -lastdig
 if lastdig > 5:
     case = "and is greater than 5"
 elif lastdig == 0:
