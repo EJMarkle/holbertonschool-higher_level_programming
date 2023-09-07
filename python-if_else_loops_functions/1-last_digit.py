ea3d410ebc3d7c12fa8dba64b26bd4b4abd8306b
EJMarkle <emarkle99@gmail.com>
2023-09-07 12:11:59 -0700

    1?

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import random
+number = random.randint(-10000, 10000)
+
+lastdig = number % 10
+
+if lastdig > 5:
+    case = "and is greater than 5"
+elif lastdig == 0:
+    case = "and is 0"
+else:
+    case = "and is less than 6 but not 0"
+
+print(f"Last digit of {number} is {lastdig} {case}")
