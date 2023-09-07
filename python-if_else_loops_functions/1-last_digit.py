53ce84b60db47050a314b82affb0fbae348cf80e
EJMarkle <emarkle99@gmail.com>
2023-09-07 12:34:02 -0700

    1...pls

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -3,15 +3,13 @@
 import random
 number = random.randint(-10000, 10000)
 
-absnum = abs(number)
-
-lastdig = absnum % 10
+lastdig = number % 10
 
 if lastdig > 5:
     case = "and is greater than 5"
 elif lastdig == 0:
     case = "and is 0"
-elif lastdig < 6 and lastdig !=0:
+else:
     case = "and is less than 6 but not 0"
 
 print(f"Last digit of {number} is {lastdig} {case}")
