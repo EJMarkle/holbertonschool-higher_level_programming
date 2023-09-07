c53941f39d6dca3e1d056d1b2ab23f8499e6293b
EJMarkle <emarkle99@gmail.com>
2023-09-07 12:29:27 -0700

    1 fix!

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -11,7 +11,7 @@ if lastdig > 5:
     case = "and is greater than 5"
 elif lastdig == 0:
     case = "and is 0"
-elif lastdig < 6 and !=0:
+elif lastdig < 6 and lastdig !=0:
     case = "and is less than 6 but not 0"
 
 print(f"Last digit of {number} is {lastdig} {case}")
