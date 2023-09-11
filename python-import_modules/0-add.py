edfaea96c990a04d4b22c58ca973d4d2449bdf77
EJMarkle <emarkle99@gmail.com>
2023-09-11 15:54:15 -0700

    fix???

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -6,7 +6,7 @@ from add_0 import add
 def main():
     a = 1
     b = 2
-    print(f"{} + {} = {}".format(a, b, add(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
 
 
 if __name__ == "__main__":
