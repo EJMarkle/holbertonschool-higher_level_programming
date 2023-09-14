1cea53875fff9e0867f29f9d9d52ef6b60f7da81
EJMarkle <emarkle99@gmail.com>
2023-09-14 15:54:16 -0700

    Docu

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,7 +2,9 @@
 # 0-square.py
 # Evan Markle
 # usage:
-# This is a simple class that will be built on to
+"""This is a simple class that will be built on to. """
+
+
 class Square:
     """This empty class is for a square"""
 
