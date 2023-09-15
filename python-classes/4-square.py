efc53f3ce9a470a662da459384f291e3195a13b6
EJMarkle <emarkle99@gmail.com>
2023-09-14 17:05:38 -0700

    setter and getter fix

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -9,12 +9,14 @@ class Square:
     """ This is where the size attribute is updated """
     def __init__(self, size=0):
         self.__size = size
-
+    
     """ This retrieves the size of the square """
+    @property
     def size(self):
         return self.__size
 
     """ This sets the size of the square """
+    @size.setter
     def size(self, value):
         if type(value) is int:
             if value >= 0:
