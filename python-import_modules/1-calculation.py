5bb567bc53fa1b7c74b0f92b7488ec0b7e206d35
EJMarkle <emarkle99@gmail.com>
2023-09-29 15:15:45 -0700

    Calc fix

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -2,18 +2,15 @@
 
 from calculator_1 import add, sub, mul, div
 
-a = 10
-b = 5
 
-add_result = add(a, b)
-sub_result = sub(a, b)
-mul_result = mul(a, b)
-div_result = div(a, b)
+def main():
+    a = 10
+    b = 5
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
 
-print("{:d} + {:d} = {:d}".format(a, b, add_result))
-print("{:d} - {:d} = {:d}".format(a, b, sub_result))
-print("{:d} * {:d} = {:d}".format(a, b, mul_result))
-print("{:d} / {:d} = {:d}".format(a, b, div_result))
 
 if __name__ == "__main__":
     main()
