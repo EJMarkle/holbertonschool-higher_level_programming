33148262defe84f10c996095305fcd0d45c883ec
EJMarkle <emarkle99@gmail.com>
2023-09-29 13:43:01 -0700

    pow adjustments

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
 
 def pow(a, b):
-    result = 1
-
-    if b == 0:
-        return 1
-    elif b < 0:
-        a = 1 / a
-        b = abs(b)
-    for _ in range(b):
-        result *= a
-    return result
+    result = a ** b
+    formatted_result = "{:2f}".format(result)
+    return formatted_result
