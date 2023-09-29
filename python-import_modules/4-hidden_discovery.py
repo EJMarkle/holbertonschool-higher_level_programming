8ddf58c966390706e6235526c221343effad2175
EJMarkle <emarkle99@gmail.com>
2023-09-29 15:56:33 -0700

    ???

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,13 @@
+#!/usr/bin/Python3
+
+import dis
+import hidden_4
+
+
+if __name__ == "__main__":
+    module_names = dir(hidden_4)
+    sorted_names = sorted(module_names)
+
+    for name in sorted_names:
+        if not name.startswith("__"):
+            print(name)
