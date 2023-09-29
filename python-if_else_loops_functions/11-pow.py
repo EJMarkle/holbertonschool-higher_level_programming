6323cbe85490542a6b228b7c37f5d95b1de97eaf
EJMarkle <emarkle99@gmail.com>
2023-09-29 14:00:54 -0700

    pycode fix

--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -2,6 +2,6 @@
 
 def pow(a, b):
     result = a ** b
-    if isinstance(result, float) and  result.is_integer():
+    if isinstance(result, float) and result.is_integer():
         return int(result)
     return result
