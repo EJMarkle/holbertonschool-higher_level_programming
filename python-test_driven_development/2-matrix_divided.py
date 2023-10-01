7593f52e13023a61f24e80da588a8bb0b708a169
EJMarkle <emarkle99@gmail.com>
2023-10-01 16:47:15 -0700

    matrix div and tests

---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,35 @@
+#!/usr/bin/Python3
+""" 
+    Divides the elements of a matrix.
+"""
+
+def matrix_divided(matrix, div):
+    """
+    Args:
+        matrix: containing ints and floats
+        div: what matrix will be divided by
+
+    Raises:
+        TypeError: if matrix is not list of ints or floats
+        ZeroDivisionError: if div = 0
+
+    Returns: 
+        new divided matrix rounded to 2 decimal places
+    """
+
+    if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
+        raise TypeError("matrix myst be a matrix (list of lists) of integers/floats")
+    if not all(len(row) == len(matrix[0]) for row in matrix):
+        raise TypeError("each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("must be a number")
+    if div == 0:
+        raise ZeroDivisionError("cannot divide by zero")
+
+    divided_matrix = []
+
+    for row in matrix:
+            new_row = [round(elem / div, 2) for elem in row]
+            divided_matrix.append(new_row)
+
+    return divided_matrix
