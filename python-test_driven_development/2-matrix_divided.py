bdcfa62c2525ec746ecc889ca3bdfd7a0bb7c7ec
EJMarkle <emarkle99@gmail.com>
2023-10-01 17:42:32 -0700

    error messages update

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -18,13 +18,13 @@ def matrix_divided(matrix, div):
     """
 
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
-        raise TypeError("matrix myst be a matrix (list of lists) of integers/floats")
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if not all(len(row) == len(matrix[0]) for row in matrix):
-        raise TypeError("each row of the matrix must have the same size")
+        raise TypeError("Each row of the matrix must have the same size")
     if not isinstance(div, (int, float)):
         raise TypeError("must be a number")
     if div == 0:
-        raise ZeroDivisionError("cannot divide by zero")
+        raise ZeroDivisionError("division by zero")
 
     divided_matrix = []
 
