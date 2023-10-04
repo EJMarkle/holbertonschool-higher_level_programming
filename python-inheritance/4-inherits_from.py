8f31478ae12d3ccf40665c80535b9bc2d0c3be6e
root <emarkle@gmail.com>
2023-10-04 15:19:59 -0700

    4 pycode fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,7 +4,10 @@
 
 def inherits_from(obj, a_class):
     """
-    Check if obj is an instance of a class that inherited (directly or indirectly)
+    Check if obj is an instance of a class that inherited
     from the specified class a_class.
     """
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
