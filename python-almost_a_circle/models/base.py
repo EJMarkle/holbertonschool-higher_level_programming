7369db5d3d08031c70415aafbede3da561379a7c
root <emarkle@gmail.com>
2023-10-03 19:04:22 -0700

    json list_objs

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,3 +21,14 @@ class Base:
         if not list_dictionaries or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """ writes the JSON representation og list_objs to a file """
+        filename = cls.__name__ + ".json"
+        json_list = []
+        if list_objs:
+            for obj in list_objs:
+                json_list.append(obj.to_dictionary())
+        with open(filename, 'w') as file:
+            file.write(cls.to_json_string(json_list))
