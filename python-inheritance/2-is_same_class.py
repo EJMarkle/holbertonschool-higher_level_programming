ad05cd289b2324677d4d11f9d6f87291f36a55a1
root <emarkle@gmail.com>
2023-10-04 15:22:13 -0700

    2 docu

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" defines a function to check if an object is in a class """
+
+
 def is_same_class(obj, a_class):
     """Check if obj is an instance of a_class."""
     return type(obj) is a_class
