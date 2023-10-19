13db0b085410884fb0d4312e716536e46dd6e2ae
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-19 18:48:41 -0500

    formatting

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
---This will create the database 'hbtn_0c_0', if it doesn't already exist.
+-- This will create the database 'hbtn_0c_0', if it doesn't already exist.
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
