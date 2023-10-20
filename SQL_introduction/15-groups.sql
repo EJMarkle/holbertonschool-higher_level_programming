b027acb170ac5e6fb4a7f46015f9414170277b19
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-20 15:29:10 -0500

    format fix

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Lists number of records with the same score in second_table. 
-SELECT name, COUNT(*) AS NUM FROM second_table GORUP BY score ORDER BY DESC;
+SELECT name, COUNT(*) AS NUM FROM second_table GORUP BY score ORDER BY score DESC;
