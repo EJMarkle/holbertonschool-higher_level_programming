3f9a9a7b21d7ba94116aac1dad37b186e787f0fc
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-20 15:16:06 -0500

    formatting

--- SQL_introduction/14-average.sql
+++ SQL_introduction/14-average.sql
@@ -1,3 +1,5 @@
 -- Computes the average score in second_table. 
-ALTER TABLE second_table ADD COLUMN average DECIMAL(10,4);
-UPDATE second_table SET average = (SELECT AVG(score) FROM second_table);
+ALTER TABLE second_table 
+ADD COLUMN average DECIMAL(10,4);
+UPDATE second_table 
+SET average = (SELECT AVG(score) FROM second_table);
