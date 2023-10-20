4e6e36c2644888375dd73035d21a988575821107
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-20 13:57:22 -0500

    rm db

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
 -- Lists all records of 'second_table' inside 'hbtn_0c_0' ordered by score in descending order
 SELECT score, name
-FROM hbtn_0c_0.second_table
+FROM second_table
 ORDER BY score DESC;
