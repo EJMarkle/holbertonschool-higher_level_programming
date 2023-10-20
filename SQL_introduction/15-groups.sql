99470d12546c803cee5396a30f0c77f04f708f29
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-20 15:43:42 -0500

    num fix

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,4 +1,4 @@
 -- Lists number of records with the same score in second_table. 
-SELECT score, COUNT(*) AS NUM 
+SELECT score, COUNT(*) AS number
 FROM second_table GROUP BY score 
-ORDER BY score DESC;
+ORDER BY number DESC, score DESC;
