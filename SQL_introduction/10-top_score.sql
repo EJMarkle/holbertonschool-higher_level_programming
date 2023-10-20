96d1dcac2dc0669b4afe6a8fd3a5d98ef28b1e3b
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-20 13:53:27 -0500

    sort by score

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,3 @@
--- Lists all records of 'second_table' inside 'hbtn_0c_0'
-SELECT name, score FROM hbtn_0c_0.second_table
+-- Lists all records of 'second_table' inside 'hbtn_0c_0' ordered by score in descending order
+SELECT name, score FROM hbtn_0c_0.second_table;
+ORDER BY score DESC;
