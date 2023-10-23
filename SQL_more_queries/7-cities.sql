126aeaf754ccbad27e22ea5644aed2dd2d516475
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-23 10:11:54 -0500

    key fix and typo

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -3,8 +3,8 @@ CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
     id INT NOT NULL AUTO_INCREMENT,
-    state_id INT N0T NULL,
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (id) REFERENCES states(id)
+    FOREIGN KEY (state_xid) REFERENCES states(id)
 )
