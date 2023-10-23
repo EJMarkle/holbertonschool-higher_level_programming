033e8e4c62713b391685b2a7b11c3e737c82e0db
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-23 10:18:13 -0500

    fixed table to pull from

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,3 @@
 -- Lists all the cities of California that can be found in the database hbtn_0d_usa.
-SELECT name = California FROM states;
+SELECT name = California FROM cities;
 ORDER BY cities.id;
