586f93ce729120eff85cc550d8efe39836b0b31a
EJMarkle <129889001+EJMarkle@users.noreply.github.com>
2023-10-23 16:01:27 -0500

    rm comments

--- python-object_relational_mapping/0-select_states copy.py
+++ python-object_relational_mapping/0-select_states copy.py
@@ -4,6 +4,7 @@
 import MySQLdb
 import sys
 
+""" Connect to MySQL database """
 if __name__ == "__main__":
     db = MySQLdb.connect(
         host="localhost",
@@ -11,12 +12,16 @@ if __name__ == "__main__":
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3])
+    """ Set up cursor to navigate database """
     cursor = db.cursor()
 
+    """ SQL Query """
     cursor.execute("SELECT * FROM states")
     states = cursor.fetchall()
 
+    """ Display retrieved states """
     for state in states:
         print(state)
+    """ Close connections """
     cursor.close()
     db.close()
